9d825c9be7c28d2e37ff3f33d1f9f1a3127e7e32
tsuru1130 <p5.novthirty.89@gmail.com>
2016-07-11 13:52:28 +0900

    FileReader/FileWriter修正

--- FileReader.java
+++ FileReader.java
@@ -8,18 +8,55 @@ class Main{
 		FileReader fw = new FileReader( "/Users/tsuru/Desktop/test.txt" );
 		
 		// FileReaderは複数の文字を一気に"読み取る"メソッドが存在しない(ストリーム概念)
+		// FileReaderは本当はFileNotFoundExceptionという例外が発生する可能性があるため、その対策も必要
 		// 基本的には1文字ずつ読み込む
-		// read()の返り値はint型
+		// read()の返り値はint型(文字コードとして返す/キャストで文字に変換できる)
 		// read()・・・次の１文字を正しく読み込めたら、その文字情報を返す
+		// "read"メソッドは"IOException"と言う例外を処理しなければいけない
 		// 最後まで読み込みが終わった場合は-1を返す
-		int i = fw.read(); // 1文字読む
+		int i = fw.read(); // 1文字読む ①
 		
 		// これ以上読めるデータがない場合は-1
 		while( i != -1 ) {
 			char c = ( char ) i;
 			System.out.print( c );
+			
+			// read()は一文字読み込んだら、自動的に次の文字へ移動する
+			// 従って↓で次の１文字をキャッチしてあげないとwhile()で常に①で代入された一文字が判定され、無限ループになる
 			i = fw.read();
 		}
 		fw.close();
+	}
+}
+
+/*
+	
+	別の書き方(tryとか書いただけ)
+	
+*/
+package aaa;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+class Main{
+	public static void main( String[] args ){
+		try {
+			FileReader fr = new FileReader( "/Users/tsuru/Desktop/test.txt" );
+			try {
+				int i = fr.read();
+				while( i != -1 ) {
+					char c = ( char ) i;
+					System.out.print( c );
+					i = fr.read();
+				}
+				
+			} catch ( Exception e ) {
+				e.getMessage();
+			}
+			
+		} catch ( FileNotFoundException e ) {
+			e.getMessage();
+		}
 	}
 }
--- FileWriter.java
+++ FileWriter.java
@@ -8,16 +8,56 @@ class Main{
 	// ファイルに書き込みする方法
 	// ・ランダムアクセス : 自由/遅い
 	// ・シーケンシャルアクセス : 先頭から順番/早い/基本
+	// JVMの外部ファイルは容量がどれだけあるかが不明なためシーケンシャルアクセス(少しずつ読み込む)が基本
 	
-	public static void main( String[] args ) throws IOException{
-		// FileWriterをインスタンス化する際にファイルを開く
-		// ↓ true追記 false上書き
-		FileWriter fw = new FileWriter( "/Users/tsuru/Desktop/test.txt", false );
-		fw.write( "XXXX" );
-		// flush()は強制書き込み
-		// ↑は必ず実行させる/ファイルに正しくデータが書き込みされない場合がある
-		// write()は書き込みの予約(バッファで溜めてる)をするだけで、必ずしも実行されるとは限らない
-		fw.flush();
-		fw.close();
+	public static void main( String[] args ){
+		// tryブロック外でnullで初期化しないとfinallyブロックでclose()を呼べない
+		FileWriter fw = null;
+		try {
+			// FileWriterをインスタンス化する際にファイルを開く
+			// ↓ true追記 false上書き
+			fw = new FileWriter( "/Users/tsuru/Desktop/test.txt", false );
+			fw.write( "XXXX" );
+			// flush()は強制書き込み
+			// ↑は必ず実行させる/ファイルに正しくデータが書き込みされない場合がある
+			// write()は書き込みの予約(バッファで溜めてる)をするだけで、必ずしも実行されるとは限らない
+			fw.flush();
+			
+		} catch ( IOException e ) {
+			e.getMessage();
+			
+		} finally { //ファイルを閉じるためにfinally{}
+			if( fw != null ) {
+				try {
+					fw.close();
+					
+				} catch ( IOException e2 ) {
+					
+				}
+			}
+		}
+	}
+}
+
+/*
+	
+	Java7以降のエレガントな書き方
+	
+*/
+package aaa;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Main{
+	public static void main( String[] args ){
+		// Java7以降はtry(){}の()の中でファイルをインスタンス化すると自動的にclose()処理が行われる
+		try( FileWriter fw = new FileWriter( "/Users/tsuru/Desktop/test.txt", true ) ) {
+			fw.write( "aaaa" );
+			fw.flush();
+			
+		} catch ( IOException e ) {
+			e.getMessage();
+		}
 	}
 }
